2a254d6f28fbe9c17bf2884c9d2c1938bb57d7cd
Fernando Oliveira <primoflro@gmail.com>
2022-08-05 15:43:50 -0300

    Requisito 1, desenvolvido em grupo

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,20 @@
 export async function getCategories() {
   // Implemente aqui
+  const response = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const data = await response.json();
+  return data;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  if(categoryId) {
+    const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`);
+    const data = await response.json();
+    return data;
+  }
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
+    const data = await response.json();
+    return data;
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  // https://api.mercadolibre.com/sites/MLB/search?category=$CATEGORY_ID&q=$QUERY
 }
+
