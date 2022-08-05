14c17307868559ba5c53311cce25dbcaa662b9b8
Sheifer Kepler <sheifer.kda@gmail.com>
2022-08-05 20:35:24 -0300

    requisito 5 - busca produtos na api, pelo input e o botão

--- src/services/api.js
+++ src/services/api.js
@@ -6,12 +6,17 @@ export async function getCategories() {
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  if (categoryId) {
+  if (categoryId.length === 0) {
+    const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
+    const data = await response.json();
+    return data;
+  }
+  if (query.length === 0) {
     const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`);
     const data = await response.json();
     return data;
   }
-  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}${query}`);
   const data = await response.json();
   return data;
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
