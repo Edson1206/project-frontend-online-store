53310a100b2a8da04066aa1261e42251d94de043
Fernando Oliveira <primoflro@gmail.com>
2022-08-05 15:56:56 -0300

    Correcao espaço no if

--- src/services/api.js
+++ src/services/api.js
@@ -6,7 +6,7 @@ export async function getCategories() {
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  if(categoryId) {
+  if (categoryId) {
     const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`);
     const data = await response.json();
     return data;
