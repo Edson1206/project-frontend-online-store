5e2a58f8e3bebad24e9b30316bf617e71852b56e
Sheifer Kepler <sheifer.kda@gmail.com>
2022-08-05 18:00:40 -0300

    requisito 3 - Adicionado carrinhho de compras (page: ShoppingCart)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './Pages/Home';
+import ShoppingCart from './Pages/ShoppingCart';
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
         <div>
-          <Route exact path="/" component={ Home } />
+          <Switch>
+            <Route exact path="/shoppingCart" component={ ShoppingCart } />
+            <Route exact path="/" component={ Home } />
+          </Switch>
         </div>
       </BrowserRouter>
     );
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Home extends React.Component {
   constructor() {
@@ -16,6 +17,7 @@ class Home extends React.Component {
     const { searchBar } = this.state;
     return (
       <div>
+        <Link data-testid="shopping-cart-button" to="/shoppingCart">ShoppingCart</Link>
         <form>
           <input
             onChange={ this.handleChange }
