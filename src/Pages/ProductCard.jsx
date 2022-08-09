d5e7ffeb0e0c478fc4f46ccdbc7eafa58ba07fb5
Sheifer Kepler <sheifer.kda@gmail.com>
2022-08-09 16:21:03 -0300

    sofrendo nos testes do requisito 11

--- src/Pages/ProductCard.jsx
+++ src/Pages/ProductCard.jsx
@@ -7,25 +7,62 @@ class ProductCard extends React.Component {
   constructor() {
     super();
 
-    this.state = {
-      product: {},
-      inputNumber: 0,
-    };
+    if (JSON.parse(localStorage.getItem('completeComent'))) {
+      this.state = {
+        completeComent: [...JSON.parse(localStorage.getItem('completeComent'))],
+        product: {},
+        inputNumber: 0,
+        textArea: '',
+        inputEmail: '',
+        avaliattion: '',
+      };
+    } else {
+      this.state = {
+        product: {},
+        inputNumber: 0,
+        textArea: '',
+        inputEmail: '',
+        avaliattion: '',
+        completeComent: [],
+      };
+    }
   }
 
   async componentDidMount() {
     const { match: { params: { id } } } = this.props;
     const item = await getProductsFromId(id);
-    this.setState({ product: item });
+    this.setState({
+      product: item,
+    });
   }
 
   handleChange = ({ target: { name, value } }) => {
     this.setState({ [name]: value });
   };
 
+  handleChangeRadios = ({ target: { value } }) => {
+    this.setState({ avaliattion: value });
+  };
+
+  renderComent = () => {
+    const { inputEmail, textArea, avaliattion } = this.state;
+    const coment = {
+      email: inputEmail,
+      avaliacao: avaliattion,
+      comentario: textArea,
+    };
+    this.setState((prevState) => ({
+      completeComent: [...prevState.completeComent, coment],
+      textArea: '',
+      inputEmail: '',
+      avaliattion: '',
+    }));
+  }
+
   render() {
-    const { product, inputNumber } = this.state;
+    const { product, inputNumber, inputEmail, textArea, completeComent } = this.state;
     const { salvaNoCarrinho } = this.props;
+    localStorage.setItem('completeComent', JSON.stringify(completeComent));
     return (
       <div>
         <p data-testid="product-detail-name">{ product.title }</p>
@@ -54,6 +91,120 @@ class ProductCard extends React.Component {
         >
           Adicionar ao Carrinho
         </button>
+        <form>
+          <input
+            data-testid="product-detail-email"
+            onChange={ this.handleChange }
+            value={ inputEmail }
+            type="email"
+            name="inputEmail"
+          />
+          <label htmlFor="avaliattion">
+            Avaliação
+            <input
+              data-testid="1-rating"
+              onChange={ this.handleChangeRadios }
+              value="1"
+              id="1"
+              type="radio"
+              name="avaliattion"
+            />
+            1
+            <input
+              data-testid="2-rating"
+              onChange={ this.handleChangeRadios }
+              value="2"
+              id="2"
+              type="radio"
+              name="avaliattion"
+            />
+            2
+            <input
+              data-testid="3-rating"
+              onChange={ this.handleChangeRadios }
+              value="3"
+              id="3"
+              type="radio"
+              name="avaliattion"
+            />
+            3
+            <input
+              data-testid="4-rating"
+              onChange={ this.handleChangeRadios }
+              value="4"
+              id="4"
+              type="radio"
+              name="avaliattion"
+            />
+            4
+            <input
+              data-testid="5-rating"
+              onChange={ this.handleChangeRadios }
+              value="5"
+              type="radio"
+              name="avaliattion"
+            />
+            5
+          </label>
+          <textarea
+            data-testid="product-detail-evaluation"
+            onChange={ this.handleChange }
+            value={ textArea }
+            name="textArea"
+            cols="30"
+            rows="10"
+          />
+          <button
+            onClick={ this.renderComent }
+            data-testid="submit-review-btn"
+            type="button"
+          >
+            Avaliar
+          </button>
+        </form>
+        <div>
+          {completeComent.map((coment) => (
+            <div key={ coment.email }>
+              <p data-testid="review-card-email">{ coment.email }</p>
+              <input
+                data-testid="1-rating"
+                type="radio"
+                name=""
+                value="1"
+                checked={ coment.avaliacao === '1' }
+              />
+              <input
+                data-testid="2-rating"
+                type="radio"
+                name=""
+                value="2"
+                checked={ coment.avaliacao === '2' }
+              />
+              <input
+                data-testid="3-rating"
+                type="radio"
+                name=""
+                value="3"
+                checked={ coment.avaliacao === '3' }
+              />
+              <input
+                data-testid="4-rating"
+                type="radio"
+                name=""
+                value="4"
+                checked={ coment.avaliacao === '4' }
+              />
+              <input
+                data-testid="5-rating"
+                type="radio"
+                name=""
+                value="5"
+                checked={ coment.avaliacao === '5' }
+              />
+              <p data-testid="product-detail-evaluation">{ coment.comentario }</p>
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
